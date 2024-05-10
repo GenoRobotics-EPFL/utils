c2b45795173da99cbeae694ecf656f01db6174ce
ykeren6 <yair.keren@epfl.ch>
2024-05-10 11:57:03 +0200

    added fake fastq generation to repo

--- access_genbank.py
+++ access_genbank.py
@@ -74,43 +74,78 @@ def download_database(gene_name, seqlen_start=0, seqlen_stop=-1):
         file.write(extract_database(gene_name, seqlen_start, seqlen_stop))
     return data_path
 
-def download_sequence(species, gene_name, dst, start_length=None, stop_length= None, id = None, permissive_search = True):
+def download_sequence_from_id(id: list, dst = None):
+    """
+    download sequence from GenBank through the Entrez database using unique identifiers.
+
+    Args:
+        id (list): list of IDs of sequences to download from GenBank
+        dst(str): destination file path
+
+    Returns:
+        Bio.SeqIO.FastaIO.FastaIterator: iterator over the records corresponding to the input ids
+    
+    """
+    handle = Entrez.efetch(db="nucleotide", id=id, retmode = "fasta", rettype = "fasta")
+    record_parser = SeqIO.parse(handle, "fasta")
+    if dst != None:
+        with open(dst, mode="a") as writer:
+            SeqIO.write(record_parser,writer,"fasta")
+        return SeqIO.parse(dst,"fasta")
+    return record_parser
+
+def get_species_from_ids(ids:list):
+    """
+    Return the species from GenBank entries with certain ids.
+
+    Parameters:
+        ids: list of ids to search
+
+    Returns:
+        dictionary that matches ids to organism names
+    
+    """
+    species_dict= dict()
+    for id in ids:
+        handle = Entrez.efetch(db="nucleotide", id=id, retmode = "gb", rettype = "gb")
+        records = SeqIO.parse(handle,"genbank")
+        one_record = next(records)
+        species_dict[id]=one_record.annotations["organism"]
+    return species_dict
+
+def download_sequence(species, gene_name, dst = None, start_length=None, stop_length= None, permissive_search = True):
     """
     Download sequence from GenBank through the Entrez database. 
 
     Parameters:
         species(str): name of species
         gene_name(str): name of gene
-        dst(str,Path-like): destination file path
+        dst(str,Path-like): destination file path, if None does not write to a file, only returns 
         start_length(int): minimum length of sequence
         stop_length(int): maximum length of sequence
         id(list): list of NCBi ids of sequences to download. If provided, overrides gene_name and species.
         permissive_search(bool, default = True): when True, if Advanced NCBI query returns nothing, replace it with a less precise general query.
+    
+    Returns:
+        Bio.SeqRecord.SeqRecord: sequence found from GenBank
+
     """
     
-    if id == None:
-        search_term = f"{gene_name}[Gene Name] AND {species}[Organism]"
-        if start_length!= None or stop_length!= None:
-                search_term += f" {start_length}:{stop_length}[Sequence Length]"
+
+    search_term = f"{gene_name}[Gene Name] AND {species}[Organism]"
+    if start_length!= None or stop_length!= None:
+            search_term += f" {start_length}:{stop_length}[Sequence Length]"
+    handle = Entrez.esearch(db = "nucleotide", term = search_term, retmax = 1, idtype = "acc")
+    search_result = Entrez.read(handle)
+    handle.close()
+    id = search_result["IdList"]
+    if len(id)==0 and permissive_search:
+        search_term = f"{gene_name} {species} {start_length}:{stop_length}[Sequence Length]"
         handle = Entrez.esearch(db = "nucleotide", term = search_term, retmax = 1, idtype = "acc")
         search_result = Entrez.read(handle)
         handle.close()
         id = search_result["IdList"]
-        n=0
-        for i in id:
-            n+=1
-        if n==0 and permissive_search:
-            search_term = f"{gene_name} {species} {start_length}:{stop_length}[Sequence Length]"
-            handle = Entrez.esearch(db = "nucleotide", term = search_term, retmax = 1, idtype = "acc")
-            search_result = Entrez.read(handle)
-            handle.close()
-            id = search_result["IdList"]
-    n=0
-    for i in id:
-        n+=1
-    if n==1:
-        handle = Entrez.efetch(db="nucleotide", id=id, retmode = "fasta", rettype = "fasta")
-        sequence = handle.read()
-        handle.close()
-        with open(dst, mode="a") as writer:
-            writer.write(sequence)
+    if len(id)==1:
+        return next(download_sequence_from_id(id,dst))
+    else: 
+         raise ValueError("Your search did not return any results")
